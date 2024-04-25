0a70860d4fe3ca0c9a04a3d04d5c569e42ac4393
Wesley Santos <wesley05santos@gmail.com>
2024-04-25 18:17:52 -0300

    Add Devise and create table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_211247) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_211544) do
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'uuid' for column 'id'
+
 end
