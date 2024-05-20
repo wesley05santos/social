10ac51b5c61088179b0978236f326e57fadfad18
Wesley Santos <wesley05santos@gmail.com>
2024-05-20 20:43:01 -0300

    Refact button logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def link_logout
-    @enable_link_logout = 0 + params[:enable_link_logout].to_i
+    @enable_link_logout = params[:enable_link_logout] == 'true' ? false : true
     respond_to do |format|
       format.turbo_stream {}
     end
