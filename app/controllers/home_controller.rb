63440a6c100f37734e7e4b29ed2a6c546fb33f3b
Wesley Santos <wesley05santos@gmail.com>
2024-05-22 18:25:20 -0300

    Remove desnecessary comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,5 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.turbo_stream {}
     end
-    # return render 'shared/link_logout' if @enable_link_logout == 1
-
-    # redirect_to root_path
   end
 end
