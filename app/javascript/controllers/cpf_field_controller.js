ee8eacc61de34ef1dc29f41f61e3921146b16e5e
Wesley Santos <wesley05santos@gmail.com>
2024-05-28 19:50:49 -0300

    Refact stimulus js

--- app/javascript/controllers/cpf_field_controller.js
+++ app/javascript/controllers/cpf_field_controller.js
@@ -1,11 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
+import { useDebounce } from 'stimulus-use'
 
 export default class extends Controller {
   static targets = [ "preview" ]
+  static debounces = [
+    {
+      name: 'fetch_input',
+      wait: 2000
+    }
+  ]
+
   connect() {
+    useDebounce(this)
   }
 
   fetch_input(event){
-    this.previewTarget.textContent = event.target.value
+    this.previewTarget.textContent = this.handle_input(event.target.value)
+  }
+
+  handle_input(text){
+    return text
   }
 }
