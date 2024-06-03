09648bce59cf64953524b52b14edece63dc8abd1
Wesley Santos <wesley05santos@gmail.com>
2024-06-03 19:37:01 -0300

    Add validation to CPF

--- app/javascript/controllers/cpf_field_controller.js
+++ app/javascript/controllers/cpf_field_controller.js
@@ -16,6 +16,8 @@ export default class extends Controller {
 
   fetch_input(event){
     this.previewTarget.textContent = this.handle_input(event.target.value)
+    const isCpfValid = this.validateCpf(event.target.value)
+    if (!isCpfValid) this.previewTarget.textContent = 'CPF INVÁLIDO'
   }
 
   handle_input(text){
@@ -24,9 +26,60 @@ export default class extends Controller {
     if(text.length >= 11){ text = this.inject_char(11, '-', text) }
     return text
   }
+
   inject_char(position, char, text){
-    var textTmp = text.split('')
+    let textTmp = text.split('')
     textTmp.splice(position,0, char)
     return textTmp.join('')
+  }
+
+  validateCpf(cpf){
+    let Soma = 0
+    let Resto
+
+    let strCPF = String(cpf).replace(/[^\d]/g, '')
+
+    if (strCPF.length !== 11)
+      return false
+
+    if ([
+      '00000000000',
+      '11111111111',
+      '22222222222',
+      '33333333333',
+      '44444444444',
+      '55555555555',
+      '66666666666',
+      '77777777777',
+      '88888888888',
+      '99999999999',
+      ].indexOf(strCPF) !== -1)
+      return false
+
+    for (let i=1; i<=9; i++)
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11))
+      Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+      return false
+
+    Soma = 0
+
+    for (let i = 1; i <= 10; i++)
+      Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11))
+      Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+      return false
+
+    return true
   }
 }
