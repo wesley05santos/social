98687ceb33ebc7db71820a0ec16702fcb1a1f330
Wesley Santos <wesley05santos@gmail.com>
2024-06-05 19:25:31 -0300

    FEAT-Search articles using title or content at parameter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,24 @@ class SearchController < ApplicationController
   end
 
   def search_article
-    @articles = Article.where("title ILIKE ?", "%#{params[:query]}%")
+    # Não é um modo performático
+
+    # @articles = []
+    # @types_search = ['title', 'content']
+    # @types_search.each do |type|
+    #   @articles << Article.where("#{type} ILIKE ?", "%#{params[:query]}%")
+    # end
+    # @articles = @articles.flatten
+
+
+    #Está correto apenas modo diferente de se fazer
+    # @articles = Article.where("title ILIKE :search_term OR content ILIKE :search_term", search_term: "%#{params[:query]}%")
+
+
+    @articles = Article.where("title ILIKE ?", "%#{params[:query]}%").or(
+      Article.where("content ILIKE ?", "%#{params[:query]}%")
+    )
+
+    render json: @articles
   end
 end
